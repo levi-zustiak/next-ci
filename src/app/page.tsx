02542a15e4e07a9c116820b071d68f3352eab5bf
Levi Zustiak <91484241+levi-zustiak@users.noreply.github.com>
2025-06-19 15:00:11 -0500

    fix: ts errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 
-function FailingComponent({
-  children,
-  random,
-}: {
-  children: ReactNode;
-  random: string;
-}) {
+function FailingComponent({ children }: { children: ReactNode }) {
   return <p>{children}</p>;
 }
 
@@ -15,6 +9,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <FailingComponent>hello world</FailingComponent>
         <Image
           className="dark:invert"
           src="/next.svg"
